6b962ff1006f348aafbe44cf37dbf61a17f21b58
Richard Guan <rguan72@gmail.com>
2020-01-01 00:49:18 -0500

    use HashRouter since hosted on non static site

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import "./app.css";
 import { makeStyles } from "@material-ui/core/styles";
@@ -26,11 +26,11 @@ export default function App() {
       <AppBar position="static" className={classes.bar}>
         Codenames
       </AppBar>
-      <Router>
+      <HashRouter>
         <Route exact path="/" component={Home} />
         <Route path="/lobby/:code" component={Lobby} />
         <Route path="/game" component={Board} />
-      </Router>
+      </HashRouter>
     </div>
   );
 }
