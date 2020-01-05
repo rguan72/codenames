65ae39e999ba90aac7c166018ef0b98bacd536bb
Richard Guan <rguan72@gmail.com>
2020-01-04 22:49:07 -0500

    tmp to get around limits

--- src/client/Firebase.js
+++ src/client/Firebase.js
@@ -1,6 +1,6 @@
 import * as firebase from "firebase/app";
 import "firebase/firestore";
-import * as firebaseConfig from "./firebase-config.json";
+import * as firebaseConfig from "./firebase-config-2.json";
 
 export default !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig.default)
