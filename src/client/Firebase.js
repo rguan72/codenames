d45c403acf60422d453d6ab729e2d1a4d4a5f264
Richard Guan <rguan72@gmail.com>
2020-01-05 14:06:37 -0500

    Clean up api keys

--- src/client/Firebase.js
+++ src/client/Firebase.js
@@ -1,7 +1,17 @@
 import * as firebase from "firebase/app";
 import "firebase/firestore";
-import * as firebaseConfig from "./firebase-config-2.json";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 export default !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig.default)
+  ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 const outputDirectory = "dist";
 
@@ -44,6 +45,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       favicon: "./public/favicon.ico"
-    })
+    }),
+    new Dotenv()
   ]
 };
